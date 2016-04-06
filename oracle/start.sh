588f59b83afcdba005240e2db6f2f58455b0ca65
Jose Deng <josephn814@foxmail.com>
2016-04-06 23:38:50 +0800

    Fix

--- oracle/start.sh
+++ oracle/start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+
 ORACLE_BASE=/opt/oracle; export ORACLE_BASE
-DB_HOME=$ORACLE_BASE/product/12.1.0/dbhome_1;export DB_HOME
+DB_HOME=$ORACLE_BASE/product/12.1.0/dbhome_1; export DB_HOME
 ORACLE_HOME=$DB_HOME; export ORACLE_HOME
 ORACLE_SID=ORCL; export ORACLE_SID
+DATA_PUMP_DIR=/home/oracle/data/dump; export DATA_PUMP_DIR
 PATH=$ORACLE_HOME/bin:$PATH; export PATH
 
 shutdown(){
@@ -11,26 +13,29 @@ shutdown(){
 	END=1
 }
 
-echo "SPFILE='${ORACLE_BASE}/data/dbs/spfile${ORACLE_SID}.ora'" > ${ORACLE_HOME}/dbs/init${ORACLE_SID}.ora
+echo "SPFILE='/home/oracle/data/dbs/spfile${ORACLE_SID}.ora'" > ${ORACLE_HOME}/dbs/init${ORACLE_SID}.ora
+
+lsnrctl start
+if [ ! -d "/opt/oracle/data/oradata" ]; then
+    echo "***** Creating database *****"
+    dbca -silent -responseFile /home/oracle/dbca.rsp
+else
+    echo "***** Database already exists *****"
+    echo startup\;  | sqlplus / as sysdba
+fi
+echo "***** Database ready to use. Enjoy! ;) *****"
+echo
+
+trap 'shutdown' INT TERM
 
 case "$1" in
     '')
-        lsnrctl start
-        if [ ! -d "/opt/oracle/data/oradata" ]; then
-            echo "Creating database"
-            dbca -silent -responseFile /home/oracle/dbca.rsp
-        else
-            echo "Database already exists"
-            echo startup\;  | sqlplus / as sysdba
-        fi
-            echo "Database ready to use. Enjoy! ;)"
-            trap 'shutdown' INT TERM
-            while [ "$END" == '' ]; do
-                sleep 1
-            done
-         	;;
+        while [ "$END" == '' ]; do
+            sleep 1
+        done
+        ;;
     *)
-        echo "Database is not configured. Please run '/opt/oracle/start.sh' if needed."
+        echo "Container is starting."
         $@
         ;;
 esac
