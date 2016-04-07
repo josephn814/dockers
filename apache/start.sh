82f97b7d83fe0280a63a971129bb47394cf07943
Jose Deng <josephn814@foxmail.com>
2016-04-07 16:58:35 +0800

    Fix

--- apache/start.sh
+++ apache/start.sh
@@ -4,6 +4,7 @@ HTTPD_HOME=/opt/apache ; export HTTPD_HOME
 PATH=$HTTPD_HOME/bin:$PATH ; export PATH
 
 shutdown(){
+    $HTTPD_HOME/bin/apachectl stop
 	END=1
 }
 
@@ -11,7 +12,7 @@ case "$1" in
     '')
         trap 'shutdown' INT TERM
 
-        $HTTPD_HOME/bin/httpd
+        $HTTPD_HOME/bin/apachectl start
 
         while [ "$END" == '' ]; do
 			sleep 1
