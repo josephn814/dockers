8c8917ecabe6648a02c9d32a2d2ef16bb210a548
Josedeng <josephn814@foxmail.com>
2016-04-13 22:55:59 +0800

    fix

--- sqlplus/start.sh
+++ sqlplus/start.sh
@@ -4,4 +4,20 @@ ORACLE_HOME=/opt/sqlplus ; export ORACLE_HOME
 LD_LIBRARY_PATH=$ORACLE_HOME ; export LD_LIBRARY_PATH
 PATH=$ORACLE_HOME:$PATH ; export PATH
 
-$@
+shutdown(){
+	END=1
+}
+
+case "$1" in
+    '')
+		trap 'shutdown' INT TERM
+
+		while [ "$END" == '' ]; do
+			sleep 1
+		done
+		;;
+	*)
+		echo "Container is starting......."
+		$@
+		;;
+esac
