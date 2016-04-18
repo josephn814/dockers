ac577a39c994c55ca8afd2b8ee9fe65d4df8faf6
Jose Deng <josephn814@foxmail.com>
2016-04-18 14:23:20 +0800

    fix

--- oracle/start.sh
+++ oracle/start.sh
@@ -5,8 +5,6 @@ DB_HOME=$ORACLE_BASE/product/12.1.0/dbhome_1; export DB_HOME
 ORACLE_HOME=$DB_HOME; export ORACLE_HOME
 ORACLE_SID=ORCL; export ORACLE_SID
 ORACLE_DATA_HOME=/opt/oracle/data ; export ORACLE_DATA_HOME
-DPUMPDIR=$ORACLE_DATA_HOME/dump ; export DPUMPDIR
-DATA_PUMP_DIR=DPUMPDIR ; export DATA_PUMP_DIR
 PATH=$ORACLE_HOME/bin:$PATH; export PATH
 
 shutdown(){
@@ -16,16 +14,11 @@ shutdown(){
 }
 
 echo "SPFILE='${ORACLE_DATA_HOME}/dbs/spfile${ORACLE_SID}.ora'" > ${ORACLE_HOME}/dbs/init${ORACLE_SID}.ora
-if [ ! -d $DPUMPDIR ]; then
-    mkdir -p $DPUMPDIR
-fi
 
 lsnrctl start
 if [ ! -d "${ORACLE_DATA_HOME}/oradata" ]; then
     echo "***** Creating database *****"
     dbca -silent -responseFile /home/oracle/dbca.rsp
-    echo "create or replace directory  $DATA_PUMP_DIR as  '$DPUMPDIR'" | sqlplus / as sysdba
-    echo "grant read,write on directory $DATA_PUMP_DIR to sys" | sqlplus / as sysdba
 else
     echo "***** Database already exists *****"
     echo startup\;  | sqlplus / as sysdba
--- oracle/wget.sh
+++ oracle/wget.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #
-# Generated onSat Apr 16 19:37:57 PDT 2016# Start of user configurable variables
+# Generated onSun Apr 17 23:22:12 PDT 2016# Start of user configurable variables
 #
 LANG=C
 export LANG
@@ -52,9 +52,9 @@ rm -f sso.out
 
 
 
-  $WGET  --user-agent="Mozilla/5.0" --no-check-certificate --load-cookies=$COOKIE_FILE --save-cookies=$COOKIE_FILE --keep-session-cookies "https://edelivery.oracle.com/osdc/download?fileName=V46095-01_1of2.zip&token=aFVzNHdqWkFTS0h4RlBnOWJURDRJUSE6OiF1c2VybmFtZT1FUEQtSkNMQVJLQEZPWE1BSUwuQ09NJnVzZXJJZD03NTg0MTk4JmNhbGxlcj1TZWFyY2hTb2Z0d2FyZSZjb3VudHJ5SWQ9Q04mZW1haWxBZGRyZXNzPWpjbGFya0Bmb3htYWlsLmNvbSZmaWxlSWQ9NzIxNjI4ODEmYXJ1PTE3ODQxNzUxJmFncmVlbWVudElkPTEzNTA1OTUmc29mdHdhcmVDaWRzPTE3NDIxMSZwbGF0Zm9ybUNpZHM9MzUmcHJvZmlsZUluc3RhbmNlQ2lkPTU1MTk1NSZtZWRpYUNpZD0yMjkzNjMmcmVsZWFzZUNpZD0" -O $OUTPUT_DIR/V46095-01_1of2.zip >> $LOGFILE 2>&1 
+  $WGET  --user-agent="Mozilla/5.0" --no-check-certificate --load-cookies=$COOKIE_FILE --save-cookies=$COOKIE_FILE --keep-session-cookies "https://edelivery.oracle.com/osdc/download?fileName=V46095-01_1of2.zip&token=UU05aG9seW1SRCtUK3pOeHI0dEN1dyE6OiF1c2VybmFtZT1FUEQtSkNMQVJLQEZPWE1BSUwuQ09NJnVzZXJJZD03NTg0MTk4JmNhbGxlcj1TZWFyY2hTb2Z0d2FyZSZjb3VudHJ5SWQ9Q04mZW1haWxBZGRyZXNzPWpjbGFya0Bmb3htYWlsLmNvbSZmaWxlSWQ9NzIxNjI4ODEmYXJ1PTE3ODQxNzUxJmFncmVlbWVudElkPTEzNTM0NjUmc29mdHdhcmVDaWRzPTE3NDIxMSZwbGF0Zm9ybUNpZHM9MzUmcHJvZmlsZUluc3RhbmNlQ2lkPTU1MTk1NSZtZWRpYUNpZD0yMjkzNjMmcmVsZWFzZUNpZD0" -O $OUTPUT_DIR/V46095-01_1of2.zip >> $LOGFILE 2>&1 
 
 
 
-  $WGET  --user-agent="Mozilla/5.0" --no-check-certificate --load-cookies=$COOKIE_FILE --save-cookies=$COOKIE_FILE --keep-session-cookies "https://edelivery.oracle.com/osdc/download?fileName=V46095-01_2of2.zip&token=VTRQZ2RVVFREREw1T1BMeTNmb1hLZyE6OiF1c2VybmFtZT1FUEQtSkNMQVJLQEZPWE1BSUwuQ09NJnVzZXJJZD03NTg0MTk4JmNhbGxlcj1TZWFyY2hTb2Z0d2FyZSZjb3VudHJ5SWQ9Q04mZW1haWxBZGRyZXNzPWpjbGFya0Bmb3htYWlsLmNvbSZmaWxlSWQ9NzIxNjI4OTEmYXJ1PTE3ODQxNzUxJmFncmVlbWVudElkPTEzNTA1OTUmc29mdHdhcmVDaWRzPTE3NDIxMSZwbGF0Zm9ybUNpZHM9MzUmcHJvZmlsZUluc3RhbmNlQ2lkPTU1MTk1NSZtZWRpYUNpZD0yMjkzNjMmcmVsZWFzZUNpZD0" -O $OUTPUT_DIR/V46095-01_2of2.zip >> $LOGFILE 2>&1 
+  $WGET  --user-agent="Mozilla/5.0" --no-check-certificate --load-cookies=$COOKIE_FILE --save-cookies=$COOKIE_FILE --keep-session-cookies "https://edelivery.oracle.com/osdc/download?fileName=V46095-01_2of2.zip&token=UGgzSnBISDZvNFpwSGZBczhyRXpnZyE6OiF1c2VybmFtZT1FUEQtSkNMQVJLQEZPWE1BSUwuQ09NJnVzZXJJZD03NTg0MTk4JmNhbGxlcj1TZWFyY2hTb2Z0d2FyZSZjb3VudHJ5SWQ9Q04mZW1haWxBZGRyZXNzPWpjbGFya0Bmb3htYWlsLmNvbSZmaWxlSWQ9NzIxNjI4OTEmYXJ1PTE3ODQxNzUxJmFncmVlbWVudElkPTEzNTM0NjUmc29mdHdhcmVDaWRzPTE3NDIxMSZwbGF0Zm9ybUNpZHM9MzUmcHJvZmlsZUluc3RhbmNlQ2lkPTU1MTk1NSZtZWRpYUNpZD0yMjkzNjMmcmVsZWFzZUNpZD0" -O $OUTPUT_DIR/V46095-01_2of2.zip >> $LOGFILE 2>&1 
 
