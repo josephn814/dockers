ef905519c6c14ed67ee6d1240759dafea4c1b572
Josedeng <josephn814@foxmail.com>
2016-04-26 16:13:47 +0800

    fix

--- intellij/start.sh
+++ intellij/start.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+DIRECTORY=/home/developer
+
+if [ "`ls -A $DIRECTORY`" = "" ]; then
+    chown -R developer:developer $DIRECTORY
+fi
 
 nohup /daemon -p 8888 -u josedeng > /dev/null 2>&1 &
 
-case "$1" in
-    '')
-        /opt/ide/bin/idea.sh
-        ;;
-    *)
-        $@
-        ;;
-esac
+/opt/ide/bin/idea.sh
