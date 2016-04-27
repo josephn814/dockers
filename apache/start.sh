b005e1fc39c986814504a3871c34c56dab0f3427
Josedeng <josephn814@foxmail.com>
2016-04-27 16:23:56 +0800

    fix

--- apache/start.sh
+++ apache/start.sh
@@ -3,6 +3,10 @@
 APACHE_HOME=/opt/apache ; export APACHE_HOME
 PATH=$APACHE_HOME/bin:$PATH ; export PATH
 
+if [ ! -r $APACHE_HOME/conf/httpd.conf ]; then
+    sudo chown -R developer:developer $APACHE_HOME
+fi
+
 shutdown(){
     sudo $APACHE_HOME/bin/apachectl stop
 	END=1
