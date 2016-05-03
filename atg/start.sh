e03bab526997c72d589633419a9c1fc02c12fca9
Josedeng <josephn814@foxmail.com>
2016-05-04 00:21:44 +0800

    fix

--- atg/start.sh
+++ atg/start.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+JAVA_HOME=/opt/jdk1.7.0_80 ; export JAVA_HOME
+JAVA_VM=$JAVA_HOME/bin/java ; export JAVA_VM
+DYNAMO_HOME=/opt/ATG/home ; export DYNAMO_HOME
+CLASSPATH=.:$JAVA_HOME/lib ; export CLASSPATH
+
 DIRECTORY=/opt/ATG
 PLATFORM_INSTALLER_FILE=/installer/OCPlatform11.1.bin
 ACC_INSTALLER_FILE=/installer/OCACC11.1.bin
+
 
 if [ "`ls -A $DIRECTORY`" = "" ]; then
     echo "Installing ATG."
@@ -12,6 +18,7 @@ if [ "`ls -A $DIRECTORY`" = "" ]; then
     chmod +x $PLATFORM_INSTALLER_FILE $ACC_INSTALLER_FILE
     $PLATFORM_INSTALLER_FILE -i silent -f /installer/installer.properties
     $ACC_INSTALLER_FILE -i silent -f /installer/installer.properties
+    sed -i '/-Djava.naming.factory.url.pkgs=atg.jndi.url/ a\           -Datg.dynamoclientlauncher.allowremote=true \\' /opt/ATG/home/bin/startACC
     chmod -R -t $DIRECTORY
 else
     echo "ATG has been installed."
@@ -28,6 +35,9 @@ case "$1" in
 		while [ "$END" == '' ]; do
 			sleep 1
 		done
+		;;
+	'acc')
+        $DYNAMO_HOME/bin/startACC
 		;;
 	*)
 		echo "Container is starting......."
