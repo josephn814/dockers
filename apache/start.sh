dd10ac27ebfdbcfdb1684d4c94a9bb9e1ecff7ee
Josedeng <josephn814@foxmail.com>
2016-05-10 15:07:48 +0800

    fix

--- apache/start.sh
+++ apache/start.sh
@@ -3,10 +3,6 @@
 APACHE_HOME=/opt/apache ; export APACHE_HOME
 PATH=$APACHE_HOME/bin:$PATH ; export PATH
 
-if [ ! -r $APACHE_HOME/conf/httpd.conf ]; then
-    sudo chown -R developer:developer $APACHE_HOME
-fi
-
 shutdown(){
     sudo $APACHE_HOME/bin/apachectl stop
 	END=1
@@ -22,6 +18,10 @@ case "$1" in
 			sleep 1
 		done
 		;;
+    'help')
+        echo "Cat $APACHE_HOME/conf/httpd.conf"
+        cat $APACHE_HOME/conf/httpd.conf
+        ;;
 	*)
 		echo "Container is starting......."
 		$@
