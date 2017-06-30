d7a2db85910c3478faf04c2e5836d37865de1a41
Jose Deng <josephn814@foxmail.com>
2017-06-29 18:08:59 -0700

    fix

--- soapui/start.sh
+++ soapui/start.sh
@@ -6,4 +6,8 @@ if [ "`ls -A $DIRECTORY`" = "" ]; then
     sudo chown -R developer:developer $DIRECTORY
 fi
 
+if [ ! -d "/home/developer/soapui_link" ]; then
+    sudo ln -s /opt/soapui/ /home/developer/soapui_link
+fi
+
 /opt/soapui/bin/soapui.sh
