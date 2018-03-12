635a0fb9e5c44fbb9a53442c828a91fa0ab109a8
Jose Deng <josephn814@foxmail.com>
2018-03-12 16:39:41 +0800

    fix

--- intellij-server/start.sh
+++ intellij-server/start.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
-/daemon -p 8888 -u josedeng
+nohup /daemon -p 8888 -u josedeng > /tmp/daemon.log 2>&1 &
+
+shutdown(){
+   END=1
+}
+
+case "$1" in
+    '')
+	trap 'shutdown' INT TERM
+        echo "Container Started."
+		while [ "$END" == '' ]; do
+			sleep 1
+		done
+		;;
+     *)
+	echo "Container is starting......."
+	$@
+	;;
+esac
