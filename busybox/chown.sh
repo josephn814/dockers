f1708f9b1fbe4acfbf95285d0b2e3932f5b02c5c
Jose Deng <josephn814@foxmail.com>
2018-04-03 16:51:22 +0800

    change cmd

--- busybox/chown.sh
+++ busybox/chown.sh
@@ -5,3 +5,14 @@ DIRECTORY=/home/developer
 if [ "`ls -A $DIRECTORY`" = "" ]; then
     sudo chown -R developer:developer $DIRECTORY
 fi
+
+if [ ! -e $DIRECTORY/init.sh ]; then
+    touch $DIRECTORY/init.sh
+    sudo chown -R developer:developer $DIRECTORY
+    sudo chmod a+x $DIRECTORY/init.sh
+    echo "#!/usr/bin/env bash" >> $DIRECTORY/init.sh
+fi
+
+source $DIRECTORY/init.sh
+
+$1
