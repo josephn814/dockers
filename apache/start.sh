cd1fd4fcbdf80447d60f1e3fb548bf5cbb7b909d
Jose Deng <josephn814@foxmail.com>
2018-04-06 18:59:35 +0800

    change init script

--- apache/start.sh
+++ apache/start.sh
@@ -3,9 +3,15 @@
 ROOT_HOME=/usr/local/apache2 ; export APACHE_HOME
 APACHE_CONFDIR=$ROOT_HOME/conf ; export APACHE_CONFDIR
 
+if [ ! -d $ROOT_HOME ]; then
+   mkdir -p $ROOT_HOME
+fi
+
 if [ "`ls -A $ROOT_HOME`" = "" ]; then
     ln -s /usr/lib/apache2/modules $ROOT_HOME/modules
     cp -R /etc/apache2 $ROOT_HOME/conf
+    mkdir -p $ROOT_HOME/{logs, htdocs}
+    chown -R developer:developer $ROOT_HOME/conf $ROOT_HOME/logs $ROOT_HOME/htdocs
 fi
 
 
