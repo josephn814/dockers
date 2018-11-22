bde65e8eda1e011344a075d3a80869e46244ae0e
Jose Deng <josedeng@aaxiscommerce.com>
2018-11-22 15:01:06 +0800

    fix

--- intellij-server/start.sh
+++ intellij-server/start.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-JAVA_HOME=/opt/jdk1.8.0_162 ; export JAVA_HOME
-JAVA_VM=$JAVA_HOME/bin/java ; export JAVA_VM
-CLASSPATH=.:$JAVA_HOME/lib ; export CLASSPATH
 JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8 ; export JAVA_TOOL_OPTIONS
-
-PATH=$JAVA_HOME/bin:$PATH; export PATH
 
 nohup /daemon -p 8888 -u josedeng > /tmp/daemon.log 2>&1 &
 nohup java -jar /daemon.jar > /tmp/daemon1.log 2>&1 &
