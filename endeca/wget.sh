0810b14cc5da146ef7034007de43bedac620d03c
Jose Deng <josedeng@aaxiscommerce.com>
2019-02-25 11:15:36 +0800

    fix

--- endeca/wget.sh
+++ endeca/wget.sh
@@ -17,10 +17,10 @@ WGET=/usr/bin/wget
 COOKIE_FILE=/tmp/$$.cookies
 
 # Log directory and file
-LOGDIR=.
+LOGDIR=/tmp
 LOGFILE=$LOGDIR/wgetlog-`date +%m-%d-%y-%H:%M`.log
 # Output directory and file
-OUTPUT_DIR=.
+OUTPUT_DIR=/tmp
 #
 # End of user configurable variable
 #
