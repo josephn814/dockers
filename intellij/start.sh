7ed2f1a717ba01ec7df13455e2f70d65eb4e7686
josephn814 <josephn814@foxmail.com>
2020-08-04 12:27:52 +0800

    Update start.sh

--- intellij/start.sh
+++ intellij/start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 DIRECTORY=/home/developer
+export GTK_PATH=/usr/lib/x86_64-linux-gnu/gtk-2.0
 
 if [ "`ls -A $DIRECTORY`" = "" ]; then
     sudo chown -R developer:developer $DIRECTORY
